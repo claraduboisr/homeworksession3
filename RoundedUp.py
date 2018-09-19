8f4774af2d11410b93dbfe6510c2a0a7b5f25776
claraduboisr <claraduboisr@gmail.com>
2018-09-19 14:06:28 +0200

    Start Commit

--- RoundedUp.py
+++ RoundedUp.py
@@ -1,5 +1,5 @@
 #Write a program that takes 2 numbers as input and prints the rounded-up division result
-num1 = input ("input the first number ")
-num2 = input ("input the second number ")
-print("the round up division is: ", round(int(num1)/int(num2)))
+num1 = int(input ("input the first number "))
+num2 = int (input ("input the second number "))
+print("the round up division is: ", int(num1/num2 + 0.5))
 
